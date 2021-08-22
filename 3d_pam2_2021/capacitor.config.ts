39dffcdf28db48c67d6612d945c4195118289f66
ichigo452 <josevictorninja1212@gmail.com>
2021-08-22 15:28:42 -0300

    Projeto ionic criado

---
+++ 3d_pam2_2021/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '3d_pam2_2021',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
